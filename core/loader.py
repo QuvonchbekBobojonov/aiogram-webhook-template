c62d5c6e93c7f76c589d9f3685594dec92ea3852
Quvonchbek Bobojonov <hi@moorfo.uz>
2025-11-19 20:09:16 +0500

    feat: update Python SDK version to 3.13 and adjust bot initialization

--- core/loader.py
+++ core/loader.py
@@ -1,7 +1,8 @@
 from aiogram import Dispatcher, Bot
+from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 from core.config import BOT_TOKEN
 
 dp = Dispatcher()
-bot = Bot(BOT_TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
